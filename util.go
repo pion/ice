dc966e04a9aabbd95255f9a748e8f3c054a0445e
Michael MacDonald <github@macdonald.cx>
2019-03-01 07:39:19 -0500

    Move pkg/ice to internal/ice
    Avoid exposing any non-standard API.

---
+++ util.go
@@ -0,0 +1,50 @@
+package ice
+
+import (
+	"net"
+	"sync/atomic"
+)
+
+func localInterfaces() (ips []net.IP) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return ips
+	}
+
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return ips
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			ips = append(ips, ip)
+		}
+	}
+	return ips
+}
+
+type atomicError struct{ v atomic.Value }
+
+func (a *atomicError) Store(err error) {
+	a.v.Store(struct{ error }{err})
+}
+func (a *atomicError) Load() error {
+	err, _ := a.v.Load().(struct{ error })
+	return err.error
+}
