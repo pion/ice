c642d290f0f4f5c0063524353a67d6ecfcc2bf49
aam335 <aam335@gmail.com>
2019-04-04 16:20:54 +0300

    testing TCP

---
+++ tcp_test.go
@@ -0,0 +1,15 @@
+package ice
+
+import (
+	"testing"
+)
+
+func TestPacketTCP(t *testing.T) {
+	// aAddr, _ := net.ResolveIPAddr("tcp", "127.0.0.1:10000")
+	//	bAddr, _ := net.ResolveIPAddr("tcp", "127.0.0.1:20000")
+	// aChan := newPacketTCP(aAddr)
+	//	bChan := newPacketTCP(bAddr)
+
+	// x:=test.
+
+}
