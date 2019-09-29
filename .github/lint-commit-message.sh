172564f799228432dcfd2e468c2f9caa5e14d04d
Hugo Arregui <hugo.arregui@gmail.com>
2019-09-29 20:51:12 -0300

    feat: semantic release support

--- .github/lint-commit-message.sh
+++ .github/lint-commit-message.sh
@@ -1,43 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
-display_commit_message_error() {
-cat << EndOfMessage
-$1
-
--------------------------------------------------
-The preceding commit message is invalid
-it failed '$2' of the following checks
-
-* Separate subject from body with a blank line
-* Limit the subject line to 50 characters
-* Capitalize the subject line
-* Do not end the subject line with a period
-* Wrap the body at 72 characters
-EndOfMessage
-
-     exit 1
-}
-
 lint_commit_message() {
-    if [[ "$(echo "$1" | awk 'NR == 2 {print $1;}' | wc -c)" -ne 1 ]]; then
-        display_commit_message_error "$1" 'Separate subject from body with a blank line'
+    commitTitle="$(echo $1 | head -n1)"
+
+    # ignore merge requests
+    if echo "$commitTitle" | grep -qE "^Merge branch \'"; then
+      echo "Commit hook: ignoring branch merge"
     fi
 
-    if [[ "$(echo "$1" | head -n1 | wc -m)" -gt 50 ]]; then
-        display_commit_message_error "$1" 'Limit the subject line to 50 characters'
-    fi
-
-    if [[ ! $1 =~ ^[A-Z] ]]; then
-        display_commit_message_error "$1" 'Capitalize the subject line'
-    fi
-
-    if [[ "$(echo "$1" | awk 'NR == 1 {print substr($0,length($0),1)}')" == "." ]]; then
-        display_commit_message_error "$1" 'Do not end the subject line with a period'
-    fi
-
-    if [[ "$(echo "$1" | awk '{print length}' | sort -nr | head -1)" -gt 72 ]]; then
-        display_commit_message_error "$1" 'Wrap the body at 72 characters'
+    # check semantic versioning scheme
+    if ! echo "$commitTitle" | grep -qE '^(feat|fix|docs|style|refactor|perf|test|chore)\(?(\w+|\s|\-|_)?\)?:\s\w+'; then
+      echo "Your commit title did not follow semantic versioning: $commitTitle"
+      echo "Please see https://github.com/angular/angular.js/blob/master/DEVELOPERS.md#commit-message-format"
+	  exit 1
     fi
 }
 
@@ -54,7 +30,7 @@ else
         TRAVIS_COMMIT_RANGE="${TRAVIS_COMMIT_RANGE}...$parent"
     fi
 
-    for commit in $(git rev-list ${TRAVIS_COMMIT_RANGE}); do
+    for commit in $(git rev-list ${TRAVIS_COMMIT_RANGE/.../..}); do
       lint_commit_message "$(git log --format="%B" -n 1 $commit)"
     done
 fi
