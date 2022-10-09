a931199dc1d1d43761c8283fa5c9dda18f980b93
cnderrauber <zengjie9004@gmail.com>
2022-10-09 16:30:16 +0800

    UDPMuxDefault accept unspecified addr with warn
    UDPMuxDefault accept unspecified address with a warn log to
    keep current application work.

--- udp_mux.go
+++ udp_mux.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/pion/logging"
 	"github.com/pion/stun"
+	"github.com/pion/transport/vnet"
 )
 
 // UDPMux allows multiple connections to go over a single UDP port
@@ -37,6 +38,9 @@ type UDPMuxDefault struct {
 	pool *sync.Pool
 
 	mu sync.Mutex
+
+	// for UDP connection listen at unspecified address
+	localAddrsForUnspecified []net.Addr
 }
 
 const maxAddrSize = 512
@@ -53,10 +57,34 @@ func NewUDPMuxDefault(params UDPMuxParams) (*UDPMuxDefault, error) {
 		params.Logger = logging.NewDefaultLoggerFactory().NewLogger("ice")
 	}
 
+	var localAddrsForUnspecified []net.Addr
 	if addr, ok := params.UDPConn.LocalAddr().(*net.UDPAddr); !ok {
 		return nil, errInvalidAddress
 	} else if ok && addr.IP.IsUnspecified() {
-		return nil, errListenUnspecified
+		// return nil, errListenUnspecified
+		// For unspecified addresses, the correct behavior is to return errListenUnspecified, but
+		// it will break the applications that are already using unspecified UDP connection
+		// with UDPMuxDefault, so print a warn log and create a local address list for mux.
+		params.Logger.Warn("UDPMuxDefault should not listening on unspecified address, use NewMultiUDPMuxFromPort instead")
+		var networks []NetworkType
+		switch {
+		case addr.IP.To4() != nil:
+			networks = []NetworkType{NetworkTypeUDP4}
+
+		case addr.IP.To16() != nil:
+			networks = []NetworkType{NetworkTypeUDP4, NetworkTypeUDP6}
+
+		default:
+			return nil, errInvalidAddress
+		}
+		muxNet := vnet.NewNet(nil)
+		ips, err := localInterfaces(muxNet, nil, nil, networks)
+		if err != nil {
+			return nil, err
+		}
+		for _, ip := range ips {
+			localAddrsForUnspecified = append(localAddrsForUnspecified, &net.UDPAddr{IP: ip, Port: addr.Port})
+		}
 	}
 
 	m := &UDPMuxDefault{
@@ -70,6 +98,7 @@ func NewUDPMuxDefault(params UDPMuxParams) (*UDPMuxDefault, error) {
 				return newBufferHolder(receiveMTU + maxAddrSize)
 			},
 		},
+		localAddrsForUnspecified: localAddrsForUnspecified,
 	}
 
 	go m.connWorker()
@@ -84,13 +113,18 @@ func (m *UDPMuxDefault) LocalAddr() net.Addr {
 
 // GetListenAddresses returns the list of addresses that this mux is listening on
 func (m *UDPMuxDefault) GetListenAddresses() []net.Addr {
+	if len(m.localAddrsForUnspecified) > 0 {
+		return m.localAddrsForUnspecified
+	}
+
 	return []net.Addr{m.LocalAddr()}
 }
 
 // GetConn returns a PacketConn given the connection's ufrag and network
 // creates the connection if an existing one can't be found
 func (m *UDPMuxDefault) GetConn(ufrag string, addr net.Addr) (net.PacketConn, error) {
-	if m.params.UDPConn.LocalAddr() != addr {
+	// don't check addr for mux using unspecified address
+	if len(m.localAddrsForUnspecified) == 0 && m.params.UDPConn.LocalAddr() != addr {
 		return nil, errInvalidAddress
 	}
 	m.mu.Lock()
