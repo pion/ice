56f01a2f1dea282c7fa1027e4d3e87f6acb948a3
Steffen Vogel <post@steffenvogel.de>
2022-11-12 15:00:52 +0100

    Refactor muxs to muxes
    As other locations in the code used muxes already before.

--- tcp_mux_multi.go
+++ tcp_mux_multi.go
@@ -17,14 +17,14 @@ type AllConnsGetter interface {
 // allowing users to pass multiple TCPMux instances to the ICE agent
 // configuration.
 type MultiTCPMuxDefault struct {
-	muxs []TCPMux
+	muxes []TCPMux
 }
 
 // NewMultiTCPMuxDefault creates an instance of MultiTCPMuxDefault that
 // uses the provided TCPMux instances.
-func NewMultiTCPMuxDefault(muxs ...TCPMux) *MultiTCPMuxDefault {
+func NewMultiTCPMuxDefault(muxes ...TCPMux) *MultiTCPMuxDefault {
 	return &MultiTCPMuxDefault{
-		muxs: muxs,
+		muxes: muxes,
 	}
 }
 
@@ -35,28 +35,28 @@ func NewMultiTCPMuxDefault(muxs ...TCPMux) *MultiTCPMuxDefault {
 func (m *MultiTCPMuxDefault) GetConnByUfrag(ufrag string, isIPv6 bool, local net.IP) (net.PacketConn, error) {
 	// NOTE: We always use the first element here in order to maintain the
 	// behavior of using an existing connection if one exists.
-	if len(m.muxs) == 0 {
+	if len(m.muxes) == 0 {
 		return nil, errNoTCPMuxAvailable
 	}
-	return m.muxs[0].GetConnByUfrag(ufrag, isIPv6, local)
+	return m.muxes[0].GetConnByUfrag(ufrag, isIPv6, local)
 }
 
 // RemoveConnByUfrag stops and removes the muxed packet connection
 // from all underlying TCPMux instances.
 func (m *MultiTCPMuxDefault) RemoveConnByUfrag(ufrag string) {
-	for _, mux := range m.muxs {
+	for _, mux := range m.muxes {
 		mux.RemoveConnByUfrag(ufrag)
 	}
 }
 
 // GetAllConns returns a PacketConn for each underlying TCPMux
 func (m *MultiTCPMuxDefault) GetAllConns(ufrag string, isIPv6 bool, local net.IP) ([]net.PacketConn, error) {
-	if len(m.muxs) == 0 {
+	if len(m.muxes) == 0 {
 		// Make sure that we either return at least one connection or an error.
 		return nil, errNoTCPMuxAvailable
 	}
 	var conns []net.PacketConn
-	for _, mux := range m.muxs {
+	for _, mux := range m.muxes {
 		conn, err := mux.GetConnByUfrag(ufrag, isIPv6, local)
 		if err != nil {
 			// For now, this implementation is all or none.
@@ -72,7 +72,7 @@ func (m *MultiTCPMuxDefault) GetAllConns(ufrag string, isIPv6 bool, local net.IP
 // Close the multi mux, no further connections could be created
 func (m *MultiTCPMuxDefault) Close() error {
 	var err error
-	for _, mux := range m.muxs {
+	for _, mux := range m.muxes {
 		if e := mux.Close(); e != nil {
 			err = e
 		}
