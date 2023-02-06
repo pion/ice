c8cff3a97a34ae7aab65330abb12267d003b237d
Artur Shellunts <shellunts.artur@gmail.com>
2023-02-06 10:25:21 +0100

    Rename TestPairSearch and refactor it
    1. Extract test setup code so that it is visible what exactly is tested.
    2. Removed check of checklist because that is tested by call to
        getBestAvailableCandidatePair.
    3. Use require.* instead of assert.*
    4. Removed check for leaked routines and timer. Because the test is
        super simple.

---
+++ agent_get_best_available_candidate_pair_test.go
@@ -0,0 +1,28 @@
+//go:build !js
+// +build !js
+
+package ice
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNoBestAvailableCandidatePairAfterAgentConstruction(t *testing.T) {
+	agent := setupTest(t)
+
+	require.Nil(t, agent.getBestAvailableCandidatePair())
+
+	tearDownTest(t, agent)
+}
+
+func setupTest(t *testing.T) *Agent {
+	agent, err := NewAgent(&AgentConfig{})
+	require.NoError(t, err)
+	return agent
+}
+
+func tearDownTest(t *testing.T, agent *Agent) {
+	require.NoError(t, agent.Close())
+}
