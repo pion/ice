03471f5adc0f81dca4014305c8c9be306c42ecf5
aler9 <46489434+aler9@users.noreply.github.com>
2023-04-12 19:11:25 +0200

    Fix regression with UDPMuxDefault and NAT1To1IPs
    PR #496 added a mechanism to close connections of duplicate candidates
    and avoid memory leaks. When UDPMuxDefault and NAT1To1IPs are used
    together, candidates may share the same connection, and candidates may
    be equal, since their IPs are overridden by the one specified in
    NAT1To1IPs. When a duplicate candidate is found, its connection is
    closed, making impossible to use any other candidate that shares the
    same connection.
    
    This PR fixes the issue as it prevents duplicate candidates from
    creating connections and getting their connection closed.

--- gather.go
+++ gather.go
@@ -255,6 +255,7 @@ func (a *Agent) gatherCandidatesLocalUDPMux(ctx context.Context) error { //nolin
 	}
 
 	localAddresses := a.udpMux.GetListenAddresses()
+	existingConfigs := make(map[CandidateHostConfig]struct{})
 
 	for _, addr := range localAddresses {
 		udpAddr, ok := addr.(*net.UDPAddr)
@@ -272,15 +273,24 @@ func (a *Agent) gatherCandidatesLocalUDPMux(ctx context.Context) error { //nolin
 			candidateIP = mappedIP
 		}
 
-		conn, err := a.udpMux.GetConn(a.localUfrag, udpAddr)
-		if err != nil {
-			return err
-		}
 		hostConfig := CandidateHostConfig{
 			Network:   udp,
 			Address:   candidateIP.String(),
 			Port:      udpAddr.Port,
 			Component: ComponentRTP,
+		}
+
+		// Detect a duplicate candidate before calling addCandidate().
+		// otherwise, addCandidate() detects the duplicate candidate
+		// and close its connection, invalidating all candidates
+		// that share the same connection.
+		if _, ok := existingConfigs[hostConfig]; ok {
+			continue
+		}
+
+		conn, err := a.udpMux.GetConn(a.localUfrag, udpAddr)
+		if err != nil {
+			return err
 		}
 
 		c, err := NewCandidateHost(&hostConfig)
@@ -297,6 +307,8 @@ func (a *Agent) gatherCandidatesLocalUDPMux(ctx context.Context) error { //nolin
 			closeConnAndLog(conn, a.log, fmt.Sprintf("Failed to add candidate: %s %d: %v", candidateIP, udpAddr.Port, err))
 			continue
 		}
+
+		existingConfigs[hostConfig] = struct{}{}
 	}
 
 	return nil
