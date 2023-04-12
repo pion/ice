ce835950693a54f0f1e2364f9bfa23d192159a11
aler9 <46489434+aler9@users.noreply.github.com>
2023-04-12 19:11:25 +0200

    Fix regression with UDPMuxDefault and NAT1To1IPs
    PR #496 added a mechanism to close connections of duplicate candidates
    and avoid memory leaks. When UDPMuxDefault and NAT1To1IPs are used
    together, candidates may share the same connection, and candidates may
    be equal, since their IPs are overridden by the one specified in
    NAT1To1IPs. When a duplicate candidate is found, its connection is
    closed, making impossible to use any other candidate that shares the
    same connection.
    
    This PR fixes the issue as it prevents duplicate candidates from
    creating connections and getting their connection closed.

--- gather_test.go
+++ gather_test.go
@@ -599,6 +599,51 @@ func TestTURNProxyDialer(t *testing.T) {
 	assert.NoError(t, a.Close())
 }
 
+// TestUDPMuxDefaultWithNAT1To1IPsUsage asserts that candidates
+// are given and connections are valid when using UDPMuxDefault and NAT1To1IPs.
+func TestUDPMuxDefaultWithNAT1To1IPsUsage(t *testing.T) {
+	report := test.CheckRoutines(t)
+	defer report()
+
+	lim := test.TimeOut(time.Second * 30)
+	defer lim.Stop()
+
+	conn, err := net.ListenPacket("udp4", ":0")
+	assert.NoError(t, err)
+	defer func() {
+		_ = conn.Close()
+	}()
+
+	mux := NewUDPMuxDefault(UDPMuxParams{
+		UDPConn: conn,
+	})
+	defer func() {
+		_ = mux.Close()
+	}()
+
+	a, err := NewAgent(&AgentConfig{
+		NAT1To1IPs:             []string{"1.2.3.4"},
+		NAT1To1IPCandidateType: CandidateTypeHost,
+		UDPMux:                 mux,
+	})
+	assert.NoError(t, err)
+
+	gatherCandidateDone := make(chan struct{})
+	assert.NoError(t, a.OnCandidate(func(c Candidate) {
+		if c == nil {
+			close(gatherCandidateDone)
+		} else {
+			assert.Equal(t, "1.2.3.4", c.Address())
+		}
+	}))
+	assert.NoError(t, a.GatherCandidates())
+	<-gatherCandidateDone
+
+	assert.NotEqual(t, 0, len(mux.connsIPv4))
+
+	assert.NoError(t, a.Close())
+}
+
 // Assert that candidates are given for each mux in a MultiUDPMux
 func TestMultiUDPMuxUsage(t *testing.T) {
 	report := test.CheckRoutines(t)
