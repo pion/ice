280be41d4162c04c884a1d9896bf5c6eefeb2e96
cnderrauber <zengjie9004@gmail.com>
2023-08-31 13:28:41 +0800

    Add BatchIO and load balance on ports to UDPMux
    Improve performance of UDPMux by BatchIO and load
    balance on ports

--- udp_mux_multi.go
+++ udp_mux_multi.go
@@ -4,12 +4,19 @@
 package ice
 
 import (
+	"errors"
 	"fmt"
 	"net"
+	"time"
 
 	"github.com/pion/logging"
 	"github.com/pion/transport/v2"
 	"github.com/pion/transport/v2/stdnet"
+	tudp "github.com/pion/transport/v2/udp"
+)
+
+var (
+	errPortBalanceRequireConnCount = errors.New("Port balance requires UDPMux implements MuxConnCount interface")
 )
 
 // MultiUDPMuxDefault implements both UDPMux and AllConnsGetter,
@@ -18,21 +25,108 @@ import (
 type MultiUDPMuxDefault struct {
 	muxes          []UDPMux
 	localAddrToMux map[string]UDPMux
+
+	enablePortBalance bool
+	// Manage port balance for mux that listen on multiple ports for same IP,
+	// for each IP, only return one addr (one port) for each GetListenAddresses call to
+	// avoid duplicate ip candidates be gathered for a single ice agent.
+	multiPortsAddresses []*multiPortsAddress
+}
+
+type addrMux struct {
+	addr net.Addr
+	mux  MuxConnCount
+}
+
+// each multiPortsAddress represents muxes listen on different ports of a same IP
+type multiPortsAddress struct {
+	addresseMuxes []*addrMux
+}
+
+func (mpa *multiPortsAddress) next() net.Addr {
+	leastAddr, leastConns := mpa.addresseMuxes[0].addr, mpa.addresseMuxes[0].mux.ConnCount()
+	for i := 1; i < len(mpa.addresseMuxes); i++ {
+		am := mpa.addresseMuxes[i]
+		if count := am.mux.ConnCount(); count < leastConns {
+			leastConns = count
+			leastAddr = am.addr
+		}
+	}
+	return leastAddr
+}
+
+// MultiUDPMuxOption provide options for NewMultiUDPMuxDefault
+type MultiUDPMuxOption func(*multipleUDPMuxDefaultParams)
+
+// MultiUDPMuxOptionWithPortBalance enables load balancing traffic on multiple ports belonging to the same IP
+// When enabled, GetListenAddresses will return the port with the least number of connections for each corresponding IP
+func MultiUDPMuxOptionWithPortBalance() MultiUDPMuxOption {
+	return func(params *multipleUDPMuxDefaultParams) {
+		params.portBalance = true
+	}
+}
+
+type multipleUDPMuxDefaultParams struct {
+	portBalance bool
 }
 
 // NewMultiUDPMuxDefault creates an instance of MultiUDPMuxDefault that
 // uses the provided UDPMux instances.
 func NewMultiUDPMuxDefault(muxes ...UDPMux) *MultiUDPMuxDefault {
+	mux, err := NewMultiUDPMuxDefaultWithOptions(muxes)
+	if err != nil {
+		panic(err)
+	}
+	return mux
+}
+
+// NewMultiUDPMuxDefaultWithOptions creates an instance of MultiUDPMuxDefault that
+// uses the provided UDPMux instances and options.
+func NewMultiUDPMuxDefaultWithOptions(muxes []UDPMux, opts ...MultiUDPMuxOption) (*MultiUDPMuxDefault, error) {
+	var params multipleUDPMuxDefaultParams
+	for _, opt := range opts {
+		opt(&params)
+	}
+
+	if params.portBalance {
+		for _, mux := range muxes {
+			if _, ok := mux.(MuxConnCount); !ok {
+				return nil, errPortBalanceRequireConnCount
+			}
+		}
+	}
+
 	addrToMux := make(map[string]UDPMux)
+	ipToAddrs := make(map[string]*multiPortsAddress)
 	for _, mux := range muxes {
 		for _, addr := range mux.GetListenAddresses() {
 			addrToMux[addr.String()] = mux
+
+			if params.portBalance {
+				muxCount, _ := mux.(MuxConnCount)
+				udpAddr, _ := addr.(*net.UDPAddr)
+				ip := udpAddr.IP.String()
+				if mpa, ok := ipToAddrs[ip]; ok {
+					mpa.addresseMuxes = append(mpa.addresseMuxes, &addrMux{addr, muxCount})
+				} else {
+					ipToAddrs[ip] = &multiPortsAddress{
+						addresseMuxes: []*addrMux{{addr, muxCount}},
+					}
+				}
+			}
 		}
 	}
-	return &MultiUDPMuxDefault{
-		muxes:          muxes,
-		localAddrToMux: addrToMux,
+
+	multiPortsAddresses := make([]*multiPortsAddress, 0, len(ipToAddrs))
+	for _, mpa := range ipToAddrs {
+		multiPortsAddresses = append(multiPortsAddresses, mpa)
 	}
+	return &MultiUDPMuxDefault{
+		muxes:               muxes,
+		localAddrToMux:      addrToMux,
+		multiPortsAddresses: multiPortsAddresses,
+		enablePortBalance:   params.portBalance,
+	}, nil
 }
 
 // GetConn returns a PacketConn given the connection's ufrag and network
@@ -64,8 +158,18 @@ func (m *MultiUDPMuxDefault) Close() error {
 	return err
 }
 
-// GetListenAddresses returns the list of addresses that this mux is listening on
+// GetListenAddresses returns the list of addresses that this mux is listening on,
+// if port balance enabled and there are multiple muxes listening to different ports of the same IP addr,
+// it will return the mux that has the least number of connections.
 func (m *MultiUDPMuxDefault) GetListenAddresses() []net.Addr {
+	if m.enablePortBalance {
+		addrs := make([]net.Addr, 0, len(m.multiPortsAddresses))
+		for _, mpa := range m.multiPortsAddresses {
+			addrs = append(addrs, mpa.next())
+		}
+		return addrs
+	}
+
 	addrs := make([]net.Addr, 0, len(m.localAddrToMux))
 	for _, mux := range m.muxes {
 		addrs = append(addrs, mux.GetListenAddresses()...)
@@ -76,6 +180,12 @@ func (m *MultiUDPMuxDefault) GetListenAddresses() []net.Addr {
 // NewMultiUDPMuxFromPort creates an instance of MultiUDPMuxDefault that
 // listen all interfaces on the provided port.
 func NewMultiUDPMuxFromPort(port int, opts ...UDPMuxFromPortOption) (*MultiUDPMuxDefault, error) {
+	return NewMultiUDPMuxFromPorts([]int{port}, opts...)
+}
+
+// NewMultiUDPMuxFromPorts creates an instance of MultiUDPMuxDefault that
+// listens to all interfaces and balances traffic on the provided ports.
+func NewMultiUDPMuxFromPorts(ports []int, opts ...UDPMuxFromPortOption) (*MultiUDPMuxDefault, error) {
 	params := multiUDPMuxFromPortParam{
 		networks: []NetworkType{NetworkTypeUDP4, NetworkTypeUDP6},
 	}
@@ -95,20 +205,29 @@ func NewMultiUDPMuxFromPort(port int, opts ...UDPMuxFromPortOption) (*MultiUDPMu
 		return nil, err
 	}
 
-	conns := make([]net.PacketConn, 0, len(ips))
+	conns := make([]net.PacketConn, 0, len(ports)*len(ips))
 	for _, ip := range ips {
-		conn, listenErr := params.net.ListenUDP("udp", &net.UDPAddr{IP: ip, Port: port})
-		if listenErr != nil {
-			err = listenErr
+		for _, port := range ports {
+			conn, listenErr := params.net.ListenUDP("udp", &net.UDPAddr{IP: ip, Port: port})
+			if listenErr != nil {
+				err = listenErr
+				break
+			}
+			if params.readBufferSize > 0 {
+				_ = conn.SetReadBuffer(params.readBufferSize)
+			}
+			if params.writeBufferSize > 0 {
+				_ = conn.SetWriteBuffer(params.writeBufferSize)
+			}
+			if params.batchWriteSize > 0 {
+				conns = append(conns, tudp.NewBatchConn(conn, params.batchWriteSize, params.batchWriteInterval))
+			} else {
+				conns = append(conns, conn)
+			}
+		}
+		if err != nil {
 			break
 		}
-		if params.readBufferSize > 0 {
-			_ = conn.SetReadBuffer(params.readBufferSize)
-		}
-		if params.writeBufferSize > 0 {
-			_ = conn.SetWriteBuffer(params.writeBufferSize)
-		}
-		conns = append(conns, conn)
 	}
 
 	if err != nil {
@@ -128,7 +247,7 @@ func NewMultiUDPMuxFromPort(port int, opts ...UDPMuxFromPortOption) (*MultiUDPMu
 		muxes = append(muxes, mux)
 	}
 
-	return NewMultiUDPMuxDefault(muxes...), nil
+	return NewMultiUDPMuxDefaultWithOptions(muxes, MultiUDPMuxOptionWithPortBalance())
 }
 
 // UDPMuxFromPortOption provide options for NewMultiUDPMuxFromPort
@@ -137,14 +256,16 @@ type UDPMuxFromPortOption interface {
 }
 
 type multiUDPMuxFromPortParam struct {
-	ifFilter        func(string) bool
-	ipFilter        func(ip net.IP) bool
-	networks        []NetworkType
-	readBufferSize  int
-	writeBufferSize int
-	logger          logging.LeveledLogger
-	includeLoopback bool
-	net             transport.Net
+	ifFilter           func(string) bool
+	ipFilter           func(ip net.IP) bool
+	networks           []NetworkType
+	readBufferSize     int
+	writeBufferSize    int
+	logger             logging.LeveledLogger
+	includeLoopback    bool
+	net                transport.Net
+	batchWriteSize     int
+	batchWriteInterval time.Duration
 }
 
 type udpMuxFromPortOption struct {
@@ -223,6 +344,16 @@ func UDPMuxFromPortWithNet(n transport.Net) UDPMuxFromPortOption {
 	return &udpMuxFromPortOption{
 		f: func(p *multiUDPMuxFromPortParam) {
 			p.net = n
+		},
+	}
+}
+
+// UDPMuxFromPortWithBatchWrite enable batch write for UDPMux
+func UDPMuxFromPortWithBatchWrite(batchWriteSize int, batchWriteInterval time.Duration) UDPMuxFromPortOption {
+	return &udpMuxFromPortOption{
+		f: func(p *multiUDPMuxFromPortParam) {
+			p.batchWriteSize = batchWriteSize
+			p.batchWriteInterval = batchWriteInterval
 		},
 	}
 }
