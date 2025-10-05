ed14a44c7f3c7329e347fc12ccc6e972da26e254
Adeithe <28549467+adeithe@users.noreply.github.com>
2025-10-04 22:23:04 -0400

    Update default relay-only candidate wait

---
+++ agent_config_test.go
@@ -0,0 +1,72 @@
+// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
+package ice
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAgentConfig_initWithDefaults(t *testing.T) {
+	relayAcceptanceMinWait := 5 * time.Second
+	tests := []struct {
+		name   string
+		config *AgentConfig
+		fn     func(*testing.T, *Agent)
+	}{
+		{
+			"default config",
+			&AgentConfig{},
+			func(t *testing.T, result *Agent) {
+				t.Helper()
+				assert.Equal(t, result.relayAcceptanceMinWait, defaultRelayAcceptanceMinWait)
+			},
+		},
+		{
+			"multiple relay candidate types",
+			&AgentConfig{CandidateTypes: []CandidateType{CandidateTypeHost, CandidateTypeServerReflexive}},
+			func(t *testing.T, result *Agent) {
+				t.Helper()
+				assert.Equal(t, result.relayAcceptanceMinWait, defaultRelayAcceptanceMinWait)
+			},
+		},
+		{
+			"host only candidate type",
+			&AgentConfig{CandidateTypes: []CandidateType{CandidateTypeHost}},
+			func(t *testing.T, result *Agent) {
+				t.Helper()
+				assert.Equal(t, result.relayAcceptanceMinWait, defaultRelayAcceptanceMinWait)
+			},
+		},
+		{
+			"relay only candidate type",
+			&AgentConfig{CandidateTypes: []CandidateType{CandidateTypeRelay}},
+			func(t *testing.T, result *Agent) {
+				t.Helper()
+				assert.Equal(t, result.relayAcceptanceMinWait, defaultRelayOnlyAcceptanceMinWait)
+			},
+		},
+		{
+			"relay only with relayAcceptanceMinWait set",
+			&AgentConfig{CandidateTypes: []CandidateType{CandidateTypeRelay}, RelayAcceptanceMinWait: &relayAcceptanceMinWait},
+			func(t *testing.T, result *Agent) {
+				t.Helper()
+				assert.Equal(t, result.relayAcceptanceMinWait, relayAcceptanceMinWait)
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			agent, err := NewAgent(test.config)
+			if !assert.NoError(t, err) {
+				return
+			}
+			defer func() { _ = agent.Close() }()
+			test.fn(t, agent)
+		})
+	}
+}
