c9e85c7f1bab2288093053e33527901bbd0f4427
Alessandro Toppi <toppi.ale@gmail.com>
2025-12-17 10:32:41 +0100

    Store lastSent and lastReceived as int64

--- candidate_test.go
+++ candidate_test.go
@@ -185,7 +185,7 @@ func TestCandidateLastSent(t *testing.T) {
 	require.Equal(t, candidate.LastSent(), time.Time{})
 	now := time.Now()
 	candidate.setLastSent(now)
-	require.Equal(t, candidate.LastSent(), now)
+	require.True(t, candidate.LastSent().Equal(now))
 }
 
 func TestCandidateLastReceived(t *testing.T) {
@@ -193,7 +193,7 @@ func TestCandidateLastReceived(t *testing.T) {
 	require.Equal(t, candidate.LastReceived(), time.Time{})
 	now := time.Now()
 	candidate.setLastReceived(now)
-	require.Equal(t, candidate.LastReceived(), now)
+	require.True(t, candidate.LastReceived().Equal(now))
 }
 
 func TestCandidateFoundation(t *testing.T) {
